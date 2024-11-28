4b663d86a2df1e11d9a5698f6fc709e5c5e957fc
Rydeckk <romainguiffantwork@hotmail.com>
2024-11-28 12:56:42 +0100

    Add test with more than one roll and function bestResultRolls to obtain the best result

--- index.test.ts
+++ index.test.ts
@@ -1,15 +1,16 @@
-import { yamsResultRoll  } from ".";
+import { bestResultRolls  } from ".";
 
-describe("romanNumber", () => {
+describe("bestResultRolls", () => {
     it.each([
-        { input: [2,4,6,2,4], output: 18},
-        { input: [2,2,2,2,2], output: 50},
-        { input: [2,2,2,2,4], output: 35},
-        { input: [2,2,2,1,4], output: 28},
-        { input: [2,2,2,4,4], output: 30},
-        { input: [2,1,5,4,3], output: 40},
+        { input: [[2,4,6,2,4]], output: 18},
+        { input: [[2,2,2,2,2]], output: 50},
+        { input: [[2,2,2,2,4]], output: 35},
+        { input: [[2,2,2,1,4]], output: 28},
+        { input: [[2,2,2,4,4]], output: 30},
+        { input: [[2,1,5,4,3]], output: 40},
+        { input: [[2,2,2,1,4],[2,1,5,4,3]], output: 40},
     ])
     ('Envoi un tableau de lancers $input et doit obtenir $output', ({input, output}) => {
-        expect(yamsResultRoll(input)).toBe(output);
+        expect(bestResultRolls(input)).toBe(output);
       })
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export function yamsResultRoll(rolls: Array<number>): number {
+function yamsResultRoll(roll: Array<number>): number {
     const valueDicePossible: Record<number, number> = {
         1: 0,
         2: 0,
@@ -8,7 +8,7 @@ export function yamsResultRoll(rolls: Array<number>): number {
         6: 0
     }
 
-    rolls.forEach((currentValueDice) => {
+    roll.forEach((currentValueDice) => {
         valueDicePossible[currentValueDice]++
     })
 
@@ -16,20 +16,34 @@ export function yamsResultRoll(rolls: Array<number>): number {
         return 40
     }
 
-        for (const valueDice in valueDicePossible) {
-            if (valueDicePossible[valueDice] === 3) {
-                for (const valueDice in valueDicePossible) {
-                    if (valueDicePossible[valueDice] === 2) {
-                        return 30
-                    }
+    for (const valueDice in valueDicePossible) {
+        if (valueDicePossible[valueDice] === 3) {
+            for (const valueDice in valueDicePossible) {
+                if (valueDicePossible[valueDice] === 2) {
+                    return 30
                 }
-                return 28
-            } else if (valueDicePossible[valueDice] === 4) {
-                return 35
-            } else if (valueDicePossible[valueDice] === 5) {
-                return 50
             }
+            return 28
+        } else if (valueDicePossible[valueDice] === 4) {
+            return 35
+        } else if (valueDicePossible[valueDice] === 5) {
+            return 50
         }
+    }
 
-    return rolls.reduce((sumDices, currentDiceValue) => sumDices + currentDiceValue, 0);
+    return roll.reduce((sumDices, currentDiceValue) => sumDices + currentDiceValue, 0);
+}
+
+export function bestResultRolls(rolls: Array<Array<number>>) {
+    let maxResult = 0
+
+    rolls.forEach((currentRoll) => {
+        let resultCurrentRoll = yamsResultRoll(currentRoll)
+
+        if(resultCurrentRoll > maxResult) {
+            maxResult = resultCurrentRoll
+        }
+    })
+
+    return maxResult
 }

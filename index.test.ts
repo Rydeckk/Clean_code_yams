4258b7e26cf2ebb561869f65f8d08c11714011f8
Rydeckk <romainguiffantwork@hotmail.com>
2024-11-28 12:17:47 +0100

    Add result carré

--- index.test.ts
+++ index.test.ts
@@ -4,6 +4,7 @@ describe("romanNumber", () => {
     it.each([
         { input: [2,4,6,2,4], output: 18},
         { input: [2,2,2,2,2], output: 50},
+        { input: [2,2,2,2,4], output: 35},
     ])
     ('Envoi un tableau de lancers $input et doit obtenir $output', ({input, output}) => {
         expect(yamsResultRoll(input)).toBe(output);
--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 export function yamsResultRoll(rolls: Array<number>): number {
     let rollIsYams = true
 
+    for(let i = 0; i < rolls.length; i++) {
+        let countDiceValueAppear = 1
+        for(let j = 0; j < rolls.length; j++) {
+            if(j === i) {
+                continue
+            }
+
+            if(rolls[j] === rolls[i]) {
+                countDiceValueAppear++
+            }
+        }
+        if(countDiceValueAppear === 4) {
+            return 35
+        }
+    }
+
     for(let i = 1; i < rolls.length; i++) {
         if(rolls[i] !== rolls[0]) {
             rollIsYams = false

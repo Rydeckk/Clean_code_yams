4cf9eb9ffa8f247dc7b2a5c7b1a224308741ca46
Rydeckk <romainguiffantwork@hotmail.com>
2024-11-28 12:11:55 +0100

    refactor yamsResultRoll

--- index.ts
+++ index.ts
@@ -1,19 +1,15 @@
 export function yamsResultRoll(rolls: Array<number>): number {
     let rollIsYams = true
 
-    for(let i = 0; i < rolls.length; i++) {
-        for(let j = 0; j < rolls.length; j++) {
-            if(j === i) {
-                continue
-            }
-            if(rolls[j] !== rolls[i]) {
-                rollIsYams = false
-                break
-            }
+    for(let i = 1; i < rolls.length; i++) {
+        if(rolls[i] !== rolls[0]) {
+            rollIsYams = false
+            break
         }
-        if(rollIsYams === true) {
-            return 50
-        }
+    }
+
+    if(rollIsYams === true) {
+        return 50
     }
 
     return rolls.reduce((sumDices, currentDiceValue) => sumDices + currentDiceValue, 0);
